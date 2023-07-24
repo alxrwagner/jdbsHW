92120fd3ee7e30ea35dd7c36167e0d5bbd3b0881
alxrw <alxr.wagner@gmail.com>
2023-07-24 17:56:41 +0700

    Initial commit

---
+++ src/main/java/com/example/jdbshw/JdbsHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbshw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbsHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbsHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdbshw/JdbsHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbshw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbsHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
